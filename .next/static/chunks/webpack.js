3f313f83b4fb7b88b3de05eb97b62e90fc20c1a4
Sam <w0292233@nscc.ca>
2025-03-04 10:08:59 -0400

    chore(build): initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("fc0fd9e183d1af8e")
+/******/ 		__webpack_require__.h = () => ("929b6e193cf10d54")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -760,7 +760,7 @@
 /******/ 			(function(linkTag) {
 /******/ 			                if (typeof _N_E_STYLE_LOAD === 'function') {
 /******/ 			                    const { href, onload, onerror } = linkTag;
-/******/ 			                    _N_E_STYLE_LOAD(new URL(href).pathname).then(()=>onload == null ? void 0 : onload.call(linkTag, {
+/******/ 			                    _N_E_STYLE_LOAD(href.indexOf(window.location.origin) === 0 ? new URL(href).pathname : href).then(()=>onload == null ? void 0 : onload.call(linkTag, {
 /******/ 			                            type: 'load'
 /******/ 			                        }), ()=>onerror == null ? void 0 : onerror.call(linkTag, {}));
 /******/ 			                } else {
