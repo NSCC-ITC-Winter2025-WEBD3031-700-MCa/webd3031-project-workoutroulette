feaccafc6d1087c36c22f2624ac786c90af8e76e
Sam <w0292233@nscc.ca>
2025-04-02 22:07:28 -0300

    feat: skeleton for admin page added

--- src/app/(site)/pricing/page.tsx
+++ src/app/(site)/pricing/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title:
-    "Pricing Page | Play SaaS Starter Kit and Boilerplate for Next.js",
+    "Pricing Page | Workout Roulette - randomized workouts with NextJS",
   description: "This is pricing page description",
 };
 
